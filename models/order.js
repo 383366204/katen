8a2ecc4ef1e25436c9676e9c4103cb7021e5fb68
Suen <383366204@qq.com>
2018-05-08 15:47:19 +0800

    order model

--- models/order.js
+++ models/order.js
@@ -6,17 +6,28 @@ const OrderSchema = new Schema({
         type: Schema.Types.ObjectId,
         require:true
     },
+    // status代表商品状态 1:待付款 2:待发货 3:待收货 :已完成 
+    status:{
+        type:Number,
+        require:true
+    },
     price:{
         type:Number,
         require:true
     },
     product:[{
-        type: Schema.Types.ObjectId,
-        require:true
+        name:{type:String,require:true},    
     }],
     address:{
         type:Schema.Types.ObjectId,
         required:true
+    },
+    message:{
+        type:String
+    },
+    date:{
+        type:Date,
+        default : Date.now
     }
 });
 
